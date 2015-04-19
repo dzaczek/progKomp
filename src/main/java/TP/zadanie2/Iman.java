99bd515b0f884175136c77cffd8d9f1930a259f3
dzaczek <sirjacek@gmail.com>
2015-04-19 10:09:43 +0200

    f

--- src/main/java/TP/zadanie2/Iman.java
+++ src/main/java/TP/zadanie2/Iman.java
@@ -3,10 +3,10 @@ package TP.zadanie2;
 
 public interface Iman {
 
-	void rent(String userId,String bookId, String date );
-	void renurn(String userId,String bookId, String date );
-	void addBook(String bookId,String title,String author, String date );
-	void removeBook(String bookid,String date, String text);
+	void rent(String userId,int bookId, String date );
+	void renurn(String userId,int bookId, String date );
+	void addBook(int bookId,String title,String author, String date );
+	void removeBook(int bookid,String date, String text);
 	
 }
 
--- src/main/java/TP/zadanie2/Library.java
+++ src/main/java/TP/zadanie2/Library.java
@@ -8,27 +8,29 @@ import java.util.HashMap;
 public class Library  implements Iman 	 {
 	private static List<Book> shelve=new ArrayList<Book>();
 	
-//	HashMap newmap=new HashMap<int,Books>();
-	public void rent(String userId, String bookId, String date) {
+
+
+	public void addBook(int bookId, String title, String author, String date) {
 		// TODO Auto-generated method stub
-		
+		shelve.add(bookId,new Book(bookId, title, author, date));
+	}
+
+	public void rent(String userId, int bookId, String date) {
+		// TODO Auto-generated method stub
 		
 	}
 
-	public void renurn(String userId, String bookId, String date) {
+	public void renurn(String userId, int bookId, String date) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void addBook(String title, String author, String bookid, String date) {
-		// TODO Auto-generated method stub
-		shelve.add(bookid,new Book(bookId, title, author, data));
-	}
-
-	public void removeBook(String bookid, String date, String text) {
-		// TODO Auto-generated method stub
+	public void removeBook(int bookid, String date, String text) {
 		
-
+		
+		// TODO Auto-generated method stub
+		shelve.remove(bookid);
+		
 		
 	}
 	
